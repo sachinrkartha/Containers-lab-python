cc10c4dce4a70fb388bfd27cbfd8841371033ed0
sachin <sachin.kartha1@gmail.com>
2020-08-26 13:43:27 -0400

    2nd commit

---
+++ student.py
@@ -0,0 +1,29 @@
+# question 1
+student=['sachin','alex','herman','joe','kareem','frank']
+print(student[1])
+print(student[-1])
+
+
+# question 2
+
+foods=['apple','banana','orange','grape','watermelon','kiwi']
+for food in foods:
+    print("food goes here is a good food")
+
+# question 3
+
+for i in range(-2,0):
+    print( foods[i] )
+    
+
+# question 4
+
+home_town = {
+    "city":'mangaf',
+    "state":'fahaheel',
+    "population":'1000000'
+}
+for hometown, stuff in home_town.items():
+    print("I was born in" + city() + state() + population())
+
+
